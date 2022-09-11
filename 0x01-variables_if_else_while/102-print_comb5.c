c0ab2e4bbb66affa9f6b2aeda1fbb0f57885eb84
Jude <judenwawere92@gmail.com>
2022-09-11 13:34:31 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,61 +1,35 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-
-* *
-* main - returns a combination of number
-
-* Return: return 0 if successful
-*/
-
-int main(void)
-
-{
-         int i, j;
-
-        for (i = 0; i < 100; i++)
-        {
-               for (j = 0; j < 100
-; j++)
-
-               {
-                       if (i < j)
-                       {
-
- putchar (( i / 10 ) + 48);
-
-
- putchar (( i % 10)  +48)
-
- putchar (' ');
- 
- putchar (( j / 10 ) +48);
- 
- putchar (( j % 10) + 48);
-
-            if ( i != 98 || j != 99
- )
-
-
-    {
-
-
-                    putchar(','
-
- );
-
-                   putchar( ' '
-);
-
-   }
-                         }
-
-
-                   }
-
-            }
-           
-            putchar('\n');
-            return (0);
-
-}
+#include <stdio.h> 
+  
+ /** 
+  * main - Entry point 
+  * 
+  * Return: Always 0 
+ */ 
+  
+ int main(void) 
+ { 
+         int p, q; 
+  
+         for (p = 0; p <= 98; p++) 
+         { 
+                 for (q = p + 1; q <= 99; q++) 
+                 { 
+                         putchar((p / 10) + '0'); 
+                         putchar((p % 10) + '0'); 
+                         putchar(' '); 
+                         putchar((q / 10) + '0'); 
+                         putchar((q % 10) + '0'); 
+  
+                         if (p == 98 && q == 99) 
+                                 continue; 
+  
+                         putchar(','); 
+                         putchar(' '); 
+                 } 
+         } 
+  
+         putchar('\n'); 
+  
+         return (0); 
+ }
